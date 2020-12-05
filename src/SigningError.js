7004b0248e23dd5cf5e6c6aeebb72dd499d35037
Aaron Cox <aaron@greymass.com>
2020-12-04 22:27:57 -0500

    linting

--- src/SigningError.js
+++ src/SigningError.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import { Header, Segment } from 'semantic-ui-react';
-import ReactJson from 'react-json-view';
 
 class SigningError extends Component {
   render() {
     const { error } = this.props;
-    console.log(error)
     return (
       <Segment color="red" size="large">
         <Header size="large">
